632999535d8bf62af33b7c600f653be5e023d552
Steven Huwig <shuwig@courseload.com>
2014-10-15 16:30:41 -0400

    Add data updates to server API.

--- web_server.py
+++ web_server.py
@@ -1,4 +1,7 @@
-from flask import Flask, jsonify
+import itertools
+
+from flask import Flask, abort, jsonify, request
+from sqlalchemy import bindparam
 
 import database
 
@@ -11,17 +14,33 @@ class CustomFlask(Flask):
 app = CustomFlask(__name__)
 
 
-@app.route('/data/<resource_name>')
+@app.route('/data/<resource_name>', methods=['GET', 'POST'])
 def resource(resource_name):
     con = database.engine.connect()
     table = getattr(database, resource_name)
+    if request.method == 'POST':
+        new_data = request.get_json()
+        if not new_data:
+            abort(400)
+        update_table(con, table, new_data)
     statement = table.select()
     column_names = [c.name for c in statement.columns]
     results = [dict(zip(column_names, row))
                for row in con.execute(statement).fetchall()]
-    print results
     con.close()
-    return jsonify({'results': results})
+    return jsonify({'data': results})
+
+
+def update_table(con, table, new_data):
+    pk = list(table.primary_key.columns)[0].name
+    xs1, xs2 = itertools.tee(iter(new_data))
+    def p(x):
+        return pk in x
+    new_items, changed_items = (itertools.ifilterfalse(p, xs1),
+                                itertools.ifilter(p, xs2))
+    con.execute(table.insert(), *list(new_items))
+    con.execute(table.update().where(table.c[pk] == bindparam('_id')),
+                *list(dict(_id=x[pk], **x) for x in changed_items))
 
 
 if __name__ == '__main__':
